cb3b8047f81aa377d7c36f40722b5702a07afcd1
Sayali Sunil Tandel <standel2@pace.edu>
2026-01-28 21:17:49 -0500

    update opelsoft in experience

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import jobt from '../../Assets/jobt.png';
 import bme from '../../Assets/bme.png';
 import fynt from '../../Assets/fynt.jpeg';
+import opelsoft from '../../Assets/opelsoft.png';
 import * as React from 'react';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
@@ -15,6 +16,33 @@ import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 export default function OppositeContentTimeline() {
     return (
         <Timeline position="alternate">
+                <TimelineItem>
+                <TimelineOppositeContent color="white" style={{fontWeight: 'bold'}}>
+                    July 2024- Current
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>
+                    <Card className="exp-card-view" >
+                        <Row>
+                            <Col>
+                                <Card.Img src={opelsoft} className="exp-card-img"/>
+                            </Col>
+                            <Col md={8}>
+                                <Card.Body>
+                                    <Card.Title style={{fontWeight: 'bold'}}>Software Engineer</Card.Title>
+                                    <Card.Text>
+                                    <ul>
+                                        <li>Skills - Python, GraphQL, RAG, Langchain, Docker</li>
+                                        </ul>                                    </Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </TimelineContent>
+            </TimelineItem>
             <TimelineItem>
                 <TimelineOppositeContent color="white" style={{fontWeight: 'bold'}}>
                     January 2024- March 2024
@@ -68,7 +96,6 @@ export default function OppositeContentTimeline() {
                             </Col>
                         </Row>
                     </Card>
-
                 </TimelineContent>
             </TimelineItem>
             <TimelineItem>
